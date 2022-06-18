ed04e223b62db6c632b1a3f3d192d74cdc85a762
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-06-19 02:22:19 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+<script>
+    import { Cell, Table } from 'data-table';
+
+    const columns = [
+        {
+            name: 'Name',
+            sortable: true,
+            visible: false,
+            type: 'string',
+            selectable: true,
+        },
+        {
+            name: 'Age',
+            sortable: true,
+            visible: true,
+            selectable: true,
+            type: 'number',
+        },
+    ];
+
+    const rows = [
+        {
+            name: 'John',
+            age: '30',
+        },
+        {
+            name: 'Joe',
+            age: '20',
+        },
+        {
+            name: 'Jane',
+            age: '40',
+        },
+    ];
+
+    const table = new Table();
+    table.createFromJSON(columns, rows);
+
+    const tableRows = table.rows;
+    tableRows.forEach((row) => {
+        console.log(row.toValueObject());
+    });
+</script>
+
+<main>
+    <table>
+        <thead>
+            <tr>
+                {#each table.header.cells as cell}
+                    <th>{cell.value}</th>
+                {/each}
+            </tr>
+        </thead>
+
+        <tbody>
+            {#each table.rows as row}
+                <tr>
+                    {#each row.cells as cell}
+                        <td>{cell.value}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</main>
+
+<style>
+</style>
